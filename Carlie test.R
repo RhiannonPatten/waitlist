474321deeafdfa7a79a912c9ea74ee8891c328a2
Carlie <carlie.bauer@live.vu.edu.au>
2023-03-17 17:09:36 +1100

    delete carlie test

--- Carlie test.R
+++ Carlie test.R
@@ -1,1 +1,6 @@
 hello carlie
+hi
+# version control in git
+# descriptive stats next time on agenda. 
+# list of variables for index: send to maria. 
+# Variables to create sum score for dependent also independent var
